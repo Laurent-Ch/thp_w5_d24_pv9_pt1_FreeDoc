94c4854d41eec62d7e495b78b1d8d4d8b59b5726
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-28 12:08:12 +0200

    all OK, curently at 2.1.4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+specialty_array = ["Cardiologue", "Dermatologue", "Gynécologue", "Généraliste", "Neurologue", "Pédiatre", "Pneumologue", "Urologue"]
+
+# Doctors
+100.times do 
+  doctor = Doctor.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    specialty: specialty_array[rand(0..7)],
+    zip_code: rand(1000..95900)
+  )
+end
+
+# Patients
+100.times do 
+  patient = Patient.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+end
+
+# Appointments?
+50.times do
+  appointment = Appointment.create!(
+    date: Faker::Date.between(from: '2021-01-01', to: '2021-12-31'),
+    doctor: Doctor.find(rand(1..100)), patient: Patient.find(rand(1..100))
+  )
+end
+
